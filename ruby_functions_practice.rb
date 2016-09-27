2f4f948759fead3fd6390bb850d3df7e0cd326a6
Alex Bazlinton <abazlinton@gmail.com>
2016-09-27 15:36:32 +0100

    initia commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,27 @@
+def return_10()
+	return 10
+end
+
+def add(num_1, num_2)
+	return num_1 + num_2
+end	
+
+def subtract(num_1, num_2)
+	return  num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+	return num_1 * num_2
+end
+
+def divide(top, bottom)
+	return top / bottom
+end
+
+def length_of_string( string )
+	return string.size
+end
+
+def join_string( string )
+	
+end
